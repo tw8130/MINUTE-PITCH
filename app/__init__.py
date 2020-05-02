0b6dfef8920452e8f820d359a76f1ec55497d2c1
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-02 21:44:46 +0300

    we register our main blueprint in our application factory

--- app/__init__.py
+++ app/__init__.py
@@ -14,5 +14,9 @@ def create_app(config_name):
     # Initializing flask extensions
     bootstrap.init_app(app)
 
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
 
     return app
