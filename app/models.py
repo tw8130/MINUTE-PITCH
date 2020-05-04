1c7773e669fc441300390136b15cc79db17efde3
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-04 12:49:16 +0300

    import login manager then use decorator @login_manage.user_loader that modifies the load_userfunction by passing in a user_id to the function that queries the database and gets a User with that ID

--- app/models.py
+++ app/models.py
@@ -1,6 +1,12 @@
 from . import db
+from . import login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 class User(UserMixin,db.Model):
     '''
