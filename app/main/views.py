332a8ac81eeb8a885280be2ad40963337c4db56d
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-04 15:24:29 +0300

    import the abort function, that stops a request, and returns a response according to the status code passed in. We then create a dynamic route that is handled by the profile view function

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for,abort
 from . import main
 from flask_login import login_required,current_user
 from ..models import User
@@ -11,3 +11,12 @@ def index():
     title = 'Home - Welcome to The Pitch website'
 
     return render_template('index.html', title=title)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
