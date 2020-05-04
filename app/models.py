9a4b9909e7806dc754952b1922507b5101d4548f
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-04 11:23:20 +0300

    create a role model then in users we add new column role_id to create a connection btwn roles and users using foreign key then we also define that relationship in role class with db.relationship on column users

--- app/models.py
+++ app/models.py
@@ -1,9 +1,26 @@
 from . import db
 
 class User(db.Model):
+    '''
+    User class to create new users
+    '''
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
 
     def __repr__(self):
         return f'User {self.username}'
+
+class Role(db.Model):
+    '''
+    Role class to create roles for our users
+    '''
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+
+    def __repr__(self):
+        return f'User {self.name}'
