a193ffc3c165257eaeb5b6d2eecb9ceb2442d798
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-04 16:19:08 +0300

    mport the the Mail class from the flask_mail extension and Initialize it

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_mail import Mail
 from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 login_manager = LoginManager()
@@ -13,6 +14,7 @@ login_manager.login_view = 'auth.login'
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 photos = UploadSet('photos',IMAGES)
+mail = Mail()
 
 def create_app(config_name):
 
@@ -25,6 +27,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
 
     # configure UploadSet
     configure_uploads(app,photos)
