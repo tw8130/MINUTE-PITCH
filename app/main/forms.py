6fd4687db9110e5113a021f195185c46e34db0da
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-04 22:11:04 +0300

    create pitch form with four input fields

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,3 +5,9 @@ from wtforms.validators import Required
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
+
+class PitchForm(FlaskForm):
+    pitch_title = StringField('Title', validators=[Required()])
+    content = TextAreaField('Pitch', validators=[Required()])
+    category = SelectField('Category', choices=[('Advertisement','Advertisement Pitch'),('Project','Project Pitch'),('Interview','Interview Pitch'),('Promotion','Promotion Pitch'),('Pick-up lines','Pick-up lines Pitch')], validators=[Required()])
+    submit = SubmitField('Write Pitch!')
