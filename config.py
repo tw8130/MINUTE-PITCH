cfdf00e2bdbd02bad1e69314849e1344802c535b
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-04 12:10:56 +0300

    add migration folder then create the first migration version and also add new environment named flask

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     '''
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
-     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://tabby:8130254#@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://user:8130254#@localhost/pitch'
     
 
 
---
+++ start.sh
@@ -0,0 +1,3 @@
+export SECRET_KEY='Pitchpill'
+
+python3.6 manage.py server
