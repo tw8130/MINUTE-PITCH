daddef5a4183896164a27d7354caa2acf56bf8d8
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-04 20:29:03 +0300

    create pitch model with columns to display pitches of users

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from . import db
 from . import login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
+from datetime import datetime
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -17,6 +18,8 @@ class User(UserMixin,db.Model):
     username = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True,index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
     password_hash = db.Column(db.String(255))
 
     @property
@@ -46,3 +49,23 @@ class Role(db.Model):
 
     def __repr__(self):
         return f'User {self.name}'
+
+class Pitch(db.Model):
+    __tablename__= 'pitches'
+
+    id = db.Column(db.Integer,primary_key = True)
+    title = db.Column(db.String())
+    body = db.Column(db.String())
+    category = db.Column(db.String())
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+
+    writer = db.Column(db.Integer,db.ForeignKey("users.id"))
+
+
+# class Comment(db.Model):
+#     __tablename__='comments'
+
+#     id = db.Column(db.Integer,primary_key=True)
+#     comment_content = db.Column(db.String())
+#     pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+#     user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
