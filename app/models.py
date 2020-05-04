2430c578cbf1b324ad72941e3253498ec80e8433
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-04 12:23:20 +0300

    we use the @property decorator to create a write only class property password. When we set this property we generate a password hash and pass the hashed password as a value to the pass_secure column property to save to the database.
    We raise an AttributeError to block access to the password property

--- app/models.py
+++ app/models.py
@@ -11,6 +11,18 @@ class User(db.Model):
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(255))
 
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
     def __repr__(self):
         return f'User {self.username}'
 
