8dce4c33072a368214f77f27ca6b77d2d66ec0f9
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-04 15:21:48 +0300

    create an index view page with title variable then we return our response which takes in name of our template and title variable

--- app/main/views.py
+++ app/main/views.py
@@ -2,3 +2,12 @@ from flask import render_template,request,redirect,url_for
 from . import main
 from flask_login import login_required,current_user
 from ..models import User
+
+@main.route('/')
+def index():
+    """
+    View root page function that returns the index page and its data
+    """
+    title = 'Home - Welcome to The Pitch website'
+
+    return render_template('index.html', title=title)
