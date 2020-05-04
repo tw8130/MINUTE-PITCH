b6b8f1c2f6636c04593d90bad13d26728f925773
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-04 16:25:20 +0300

    import the mail_message function then call the function inside our register view function. We pass in the the subject and template file where our message body will be stored. We then pass in the new user's email address which we get from the registration form. We then pass in a user as a keyword argument

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,6 +4,7 @@ from .forms import LoginForm, RegistrationForm
 from flask_login import login_user,logout_user,login_required
 from ..import db
 from . import auth
+from ..email import mail_message
 
 
 @auth.route('/login',methods=['GET','POST'])
@@ -35,6 +36,9 @@ def register():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
+
+        mail_message("Welcome to watchlist","email/welcome_user",user.email,user=user)
+
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
