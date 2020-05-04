63d2f27985ca357d4590181479aed45688234f24
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-04 11:07:17 +0300

    import user class then in our make_shell_context fuction we return app,db instance and user class

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Server
+from app.models import User
 
 # Creating app instance
 app = create_app('development')
@@ -17,7 +18,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db)
+    return dict(app = app,db = db,User = User)
 
 if __name__ == '__main__':
     manager.run()
