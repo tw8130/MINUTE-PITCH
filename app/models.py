8951cbd3478fa71420962deb0ddc22fb2894bc32
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-05 14:32:53 +0300

    create like model

--- app/models.py
+++ app/models.py
@@ -133,6 +133,16 @@ class Comment(db.Model):
         comments = Comment.query.order_by('id').all()
         return comments
 
+class Like (db.Model):
+    __tablename__ = 'likes'
+
+    id = db.Column(db.Integer,primary_key=True)
+    like = db.Column(db.Integer,default=1)
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+
+
+
 
 
 
