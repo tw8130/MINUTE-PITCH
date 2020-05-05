54f9880410a00325e94391c02ae8823caea4c63d
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-05 15:30:33 +0300

    create pitch,comment ,like ,dislike view functions

--- app/main/views.py
+++ app/main/views.py
@@ -24,12 +24,14 @@ def profile(uname):
 
     get_pitches = Pitch.query.filter_by(author = User.id).all()
     get_comments = Comment.query.filter_by(user_id = User.id).all()
+    get_likes = Like.query.filter_by(user_id = User.id).all()
+    get_dislikes = Dislike.query.filter_by(user_id = User.id).all()
 
 
     if user is None:
         abort(404)
 
-    return render_template("profile/profile.html", user = user,pitches_no = get_pitches, comments_no = get_comments)
+    return render_template("profile/profile.html", user = user,pitches_no = get_pitches, comments_no = get_comments,likes_no = get_likes,dislikes_no = get_dislikes)
 
 @main.route('/user/<uname>/update',methods = ['GET','POST'])
 @login_required
@@ -81,3 +83,89 @@ def index2():
 
     title = 'Home | One Min Pitch'
     return render_template('home.html', title = title, pitch = pitch, advertisement = advertisement, project = project, general = general, sale = sale)
+
+@main.route('/pitch/new',methods = ['GET','POST'])
+@login_required
+def pitch():
+    '''
+    View pitch function that returns the pitch page and data
+    '''
+    pitch_form = PitchForm()
+    likes = Like.query.filter_by(pitch_id=Pitch.id)
+
+    if pitch_form.validate_on_submit():
+        body = pitch_form.body.data
+        category = pitch_form.category.data
+        title = pitch_form.title.data
+
+        new_pitch = Pitch(title=title, body=body, category = category, user = current_user)
+        new_pitch.save_pitch()
+
+        return redirect(url_for('main.index'))
+
+
+    title = 'New Pitch | One Minute Pitch'
+    return render_template('pitch.html', title = title, pitch_form = pitch_form, likes = likes)
+
+
+@main.route('/pitch/<int:pitch_id>/comment',methods = ['GET', 'POST'])
+@login_required
+def comment(pitch_id):
+    '''
+    View comments page function that returns the comment page and its data
+    '''
+
+    comment_form = CommentForm()
+    pitch = Pitch.query.get(pitch_id)
+    if pitch is None:
+        abort(404)
+
+    if comment_form.validate_on_submit():
+        comment_body = comment_form.comment_body.data
+
+        new_comment = Comment(comment=comment_body, pitch_id = pitch_id, user = current_user)
+        new_comment.save_comment()
+
+        return redirect(url_for('.comment', pitch_id=pitch_id))
+
+    comments = Comment.query.filter_by(pitch_id=pitch_id).all()
+    title = 'New Comment | One Minute Pitch'
+
+    return render_template('comment.html', title = title, pitch=pitch ,comment_form = comment_form, comment = comments )
+
+@main.route('/pitch/<int:pitch_id>/like',methods = ['GET','POST'])
+@login_required
+def like(pitch_id):
+    '''
+    View like function that returns likes
+    '''
+    pitch = Pitch.query.get(pitch_id)
+    user = current_user
+
+    likes = Like.query.filter_by(pitch_id=pitch_id)
+
+
+    if Like.query.filter(Like.user_id==user.id,Like.pitch_id==pitch_id).first():
+        return  redirect(url_for('.index'))
+
+    new_like = Like(pitch_id=pitch_id, user = current_user)
+    new_like.save_likes()
+    return redirect(url_for('.index'))
+
+@main.route('/pitch/<int:pitch_id>/dislike',methods = ['GET','POST'])
+@login_required
+def dislike(pitch_id):
+    '''
+    View dislike function that returns dislikes
+    '''
+    pitch = Pitch.query.get(pitch_id)
+    user = current_user
+
+    pitch_dislikes = Dislike.query.filter_by(pitch_id=pitch_id)
+
+    if Dislike.query.filter(Dislike.user_id==user.id,Dislike.pitch_id==pitch_id).first():
+        return redirect(url_for('.index'))
+
+    new_dislike = Dislike(pitch_id=pitch_id, user = current_user)
+    new_dislike.save_dislikes()
+    return redirect(url_for('.index'))
